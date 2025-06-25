492129cc8d204790511555e49b8b1f286350ea13
Yash Raj Singh <workmail.yrs@gmail.com>
2025-06-26 01:43:27 +0530

    useRef explained

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  // Useref explained 
+  const inputRef = useRef();
+  function inputHandler () {
+    inputRef.current.focus();
+  }
 
   return (
     <>
-      
+      <div>
+        <input ref={inputRef} type="text" placeholder='name'></input>
+        <input type="password" placeholder='password'></input>
+        <button onClick={inputHandler}>Enter</button>
+      </div>
     </>
   )
 }
