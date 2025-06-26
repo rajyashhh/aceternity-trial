812ec445e94dafd4f78d17a9c879d6616fcac82b
Yash Raj Singh <workmail.yrs@gmail.com>
2025-06-26 15:06:53 +0530

    useRef usecases

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,26 @@ import reactLogo from './assets/react.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-  // Useref explained 
-  const inputRef = useRef();
-  function inputHandler () {
-    inputRef.current.focus();
+  const [value, setValue] = useState(0);
+  const timer = useRef();
+  function startTimer (){
+    let value = setInterval(function(){
+      setValue(x=>x+1)
+    },1000)
+    timer.current = value
   }
-
+  function stopTimer (){
+    clearInterval(timer.current)
+  }
   return (
-    <>
-      <div>
-        <input ref={inputRef} type="text" placeholder='name'></input>
-        <input type="password" placeholder='password'></input>
-        <button onClick={inputHandler}>Enter</button>
-      </div>
-    </>
+    <div>
+      {value}
+      <br></br>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+    </div>
+    
+    
   )
 }
 
